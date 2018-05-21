c431e0b652ac0b2888adff05e24ec219643122e5
Evan <timmsevan@gmail.com>
2018-05-20 21:00:47 -0600

    added basic outline to popup

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
 chrome.runtime.onInstalled.addListener(function() {
     console.log("Extension Installed");
   });
+
+// added listener for content script message
+
+chrome.runtime.onMessage.addListener(
+  function(request,sender, sendResponse){
+    console.log("messaged received");
+});
--- content.js
+++ content.js
@@ -1,1 +1,18 @@
-console.log("Chrome Extension Loaded");
+//grab current url and pass to background script
+
+var curr_url;
+
+function getUrl(callBack){
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        curr_url = tabs[0].url;
+    });
+}
+
+console.log(curr_url);
+
+//sending to background script
+
+chrome.runtime.sendMessage({url: curr_url}, function(response){
+    console.log(response);
+});
+

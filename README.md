029ad71ab9151ed14232e6b99d3d792881e36a27
Evan <timmsevan@gmail.com>
2018-06-17 11:48:38 -0600

    added date tracking

--- README.md
+++ README.md
@@ -1,13 +1,3 @@
 # Budgie
 
-## TODO
-
-    *Finish Layout
-    *Interactivity for adding/modifying and deleting budgets
-    *D3 for Data Display
-    *User Authentication using OAuth2
-    *Chrome API for persistant user storage
-    *Styling with Materialize CSS
-    *Icons/Fonts
-    *"Smart Url Parsing" For faster use on webpages
-    *Profile page
+Description coming soon
--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
 //Placeholder for dynamic stuff later
-const fundsRemaining = document.getElementById("fundsRemaining");
-const daysRemaining = document.getElementById("daysRemaining");
-const resetInterval = document.getElementById("resetInterval");
+let fundsRemaining = document.getElementById("fundsRemaining");
+let daysRemaining = document.getElementById("daysRemaining");
+let resetInterval = document.getElementById("resetInterval");
+let resetDate = document.getElementById("resetDate");
+const NUMTODAY = {
+    0 : "weekly",
+    1 : "bi-weekly",
+    2 : "monthly",
+    3 : "yearly"
+}
+
+function calculateDaysLeft(str){
+    //get current date
+    let today = new Date();
+
+    let mdy = str.split('-');
+    
+    if(mdy[1][0] == 0){
+        mdy[1] = mdy[1][1];
+    }
+
+    if(mdy[0][0] == 0){
+        mdy[0] = mdy[0][1];
+    }
+    
+    let deadline = new Date(mdy[0],mdy[1]-1,mdy[2]);
+    let diff = deadline.getTime()-today.getTime();
+    let days = Math.floor(diff/(1000*60*60*24));
+    // console.log(days);
+    return days;
+}
+
 
 chrome.storage.local.get(['formResults'], function(result){
     if(result.formResults != undefined){
         fundsRemaining.textContent = result.formResults.amount;
-        resetInterval.textContent = result.formResults.resetInterval;
-        daysRemaining.textContent = result.formResults.resetDate;
+        resetInterval.textContent = NUMTODAY[result.formResults.resetInterval];
+        resetDate.textContent = result.formResults.resetDate;
+        daysRemaining.textContent =  calculateDaysLeft(result.formResults.resetDate);
     }
 });
 
